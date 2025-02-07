2242534066b28299d151b3f4478fada32a11db10
Mateusz Lewandowski <lewandowski.mattt@gmail.com>
2025-02-07 16:28:57 +0100

    Create login.py

---
+++ login.py
@@ -0,0 +1,21 @@
+import pyautogui
+import time
+
+class Login:
+    """Handles the login process."""
+    def __init__(self, driver, logger):
+        self.driver = driver
+        self.logger = logger
+
+    def login(self, email, password):
+        """Login to Swift Shop account with email and password."""
+        self.move_mouse(943, 372, 0.99, 1.98)
+        pyautogui.write(email)
+        time.sleep(1.1)
+        pyautogui.press('enter')
+
+        self.move_mouse(896, 476, 1.02, 2.01)
+        pyautogui.write(password)
+        time.sleep(0.99)
+        pyautogui.press('enter')
+        time.sleep(1.82)
